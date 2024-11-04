19b662cc3708860a5042ed9743aefa3f33962921
kelvin <tanadi96@gmail.com>
2024-11-04 16:28:11 +0700

    fix gemini

--- schema/gemini.js
+++ schema/gemini.js
@@ -4,8 +4,20 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
 
 const geminiType = `#graphql
+type gemini {
+  _id: ID
+  userTransaction: [UserTransaction]
+  name: String
+  items: [Item]
+  category:String
+  tax: Int
+  totalPrice: Float
+  userId: ID
+  createdAt: String
+  updatedAt: String
+}
 type Query{
-    gemini(location:String):String
+    gemini(location:String):gemini
 }
 `;
 
@@ -16,24 +28,31 @@ const geminiResolvers = {
       const genAI = new GoogleGenerativeAI(process.env.GOOGLE_KEY_GEMINIAI);
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-      const prompt = `Please provide a recommendation of three different types of plants suitable for growing in ${location}. Each recommendation should include:
-- "name": The plant's name.
-- "imageUrl": A URL linking to an image of the plant.
-- "type": The type of plant (e.g., herb, flowering plant, tree).
-- "description": A brief description of the plant.
-
-The response should be structured in plain text JSON format without code block markers or escape characters, like this:
-[
-  { "name": "Plant Name 1", "imageUrl": "https://example.com/image1.jpg", "type": "Type 1", "description": "Description 1" },
-  { "name": "Plant Name 2", "imageUrl": "https://example.com/image2.jpg", "type": "Type 2", "description": "Description 2" },
-  { "name": "Plant Name 3", "imageUrl": "https://example.com/image3.jpg", "type": "Type 3", "description": "Description 3" }
-]
-
-Return the response as a single JSON string.`;
+      const prompt = `please make a transaction with the following data: ${location}
+      Respond only in JSON format, structured as follows:
+  {
+        "_id": "67287f31cca8170991cbf655",
+        "name": "Nasi Uduk Emak",
+        "tax": 10,
+        "totalPrice": 28600.000000000004,
+        "userId": "6720cac36378aea6ee1b401f",
+        "createdAt": "2024-11-04T08:00:49.472Z",
+        "updatedAt": "2024-11-04T08:00:49.472Z",
+        "category": "Food",
+        "items": [
+          {
+            "name": "Nasi Uduk",
+            "price": 13000,
+            "quantity": 2
+          }
+        ],
+        "userTransaction": {}
+      },
+`
       console.log({ prompt });
       try {
         const result = await model.generateContent(prompt);
-        console.log(result.response.text(), "ini respone");
+        console.log(result.response.text().trim(), "ini respone");
 
         return JSON.parse(result.response.text().trim());
       } catch (error) {
